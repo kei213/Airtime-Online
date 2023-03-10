7261b48983360d78d472cf87ff4ef036d6e87bf9
kei213 <kzimaman@gmail.com>
2023-03-10 16:26:45 +0200

    auth

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,12 @@ const User = require("../models/User");
 
 // register a single user
 router.post("/register", async (req, res) => {
-	const { username, password, phonenumber } = req.body
 
 	// add doc to db
 	try {
+		const salt = await bcrypt.genSalt(10)
+		const hashedPassword = await bcrypt.hash(req.body.password, salt)
+		const { username, password, phonenumber } = req.body
 		const newUser = await User.create({
 			username,
 			password,
@@ -14,8 +16,28 @@ router.post("/register", async (req, res) => {
 		});
 		res.status(200).json(newUser);
 	} catch (error) {
-		res.status(400).json({ error: error.message });
+		res.status(500).json({ error: error.message });
 	}
 });
+
+// login user
+router.post("login", async (req, res) => {
+	try {
+        const user = await User.findOne({email: req.body.email})
+        if (!user) {
+        	return res.status(404).json("user not found")
+        }
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password)
+        if (!validPassword) {
+        	return res.status(400).json("wrong password")
+        }
+        res.status(200).json(user)
+        
+	} catch(error) {
+        res.status(500).json({error: error.message})
+	}
+	
+})
 
 module.exports = router;
